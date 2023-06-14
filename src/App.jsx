70976fa842208bd14e39d33c53e9907b1f9f0495
Hasanat902 <abulhasanat902@gmail.com>
2023-06-14 15:46:42 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/FooterDiv/Footer';
+import Search from './components/SearchDiv/Search';
+import Jobs from './components/JobDiv/Jobs';
+import Value from './components/ValueDiv/Value';
 
 const App = () => {
   return (
-    <div>
-      <p className='text-green-600'>THis is my app</p>
+    <div className='w-[85%] m-auto bg-white'>
+      <NavBar></NavBar>
+      <Search></Search>
+      <Jobs></Jobs>
+      <Value></Value>
+      <Footer></Footer>
     </div>
   );
 };
