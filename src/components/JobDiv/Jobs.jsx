c2739853cbcd43ac9ec5ebabe8bddff150b12d77
Hasanat902 <abulhasanat902@gmail.com>
2023-06-15 12:38:16 +0600

    added search part

--- src/components/JobDiv/Jobs.jsx
+++ src/components/JobDiv/Jobs.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div>
+      <div className="jobContainer flex items-center justify-center gap-10 flex-wrap py-10">
+
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+
+          
+
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
